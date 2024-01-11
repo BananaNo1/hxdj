5deee3ca9f23ad8435ab9cddea965caa0616c441
BananaNO1 <893052525@qq.com>
2024-01-11 23:05:38 +0800

    mps-init

---
+++ hxds-mps/src/main/java/com/leis/hxds/mps/config/SpringDocConfig.java
@@ -0,0 +1,11 @@
+package com.leis.hxds.mps.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(info = @Info(title = "hxds-mps", description = "地图子系统", version = "1.0"))
+@Configuration
+public class SpringDocConfig {
+}
+
---
+++ hxds-mps/src/test/java/com/leis/hxds/mps/HxdjMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leis.hxds.mps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxdjMapsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
