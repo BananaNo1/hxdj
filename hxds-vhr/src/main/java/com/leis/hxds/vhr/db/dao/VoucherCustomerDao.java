91d8456fdb6e9948f0a0adc245eb4acb04261ba7
leis <893052525@qq.com>
2024-01-12 16:15:32 +0800

    1-10 vhr init

---
+++ hxds-vhr/src/main/java/com/leis/hxds/vhr/db/dao/VoucherCustomerDao.java
@@ -0,0 +1,18 @@
+package com.leis.hxds.vhr.db.dao;
+
+
+import com.leis.hxds.vhr.db.pojo.VoucherCustomerEntity;
+
+import java.util.Map;
+
+public interface VoucherCustomerDao {
+    public int insert(VoucherCustomerEntity entity);
+
+    public String validCanUseVoucher(Map param);
+
+    public int bindVoucher(Map param);
+}
+
+
+
+
