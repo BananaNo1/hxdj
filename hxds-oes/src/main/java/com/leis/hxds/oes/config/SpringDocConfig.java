bd25c4e96b2509c14a16624dbc063718c9466f4e
leis <893052525@qq.com>
2024-01-12 15:30:34 +0800

    1-8 oes

---
+++ hxds-oes/src/main/java/com/leis/hxds/oes/config/SpringDocConfig.java
@@ -0,0 +1,16 @@
+package com.leis.hxds.oes.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "hxds-oes",
+                description = "订单执行子系统",
+                version = "1.0"
+        )
+)
+@Configuration
+public class SpringDocConfig {
+}
